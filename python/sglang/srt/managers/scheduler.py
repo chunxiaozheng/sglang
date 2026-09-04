d2cb0578384c7b4856d232849716564f3a3569fa
chunxiaozheng <1179548172@qq.com>
2026-09-04 11:30:10 +0800

    bugfix
    Signed-off-by: chunxiaozheng <1179548172@qq.com>

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -3876,6 +3876,7 @@ class Scheduler(
                 if res == AddReqResult.NO_TOKEN:
                     if (
                         self.enable_hierarchical_cache
+                        or self.enable_unified_lmcache
                         or self.enable_unified_cache_external_linker
                     ):
                         # Set batch_is_full after making sure there are requests that can be served
